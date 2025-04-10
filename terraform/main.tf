fda8c0eaa355e31ee50d3990151563225b248a4e
Furqaan <fnaeem2@gmail.com>
2025-04-10 17:19:23 +0100

    add vpc module to root main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
 module "vpc_module" {
   source = "./modules/vpc"
+  vpc_cidr            = "10.0.0.0/16"
+  azs                 = ["eu-west-2a", "eu-west-2b"]
+  public_subnet_cidrs = ["10.0.0.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
 module "alb_module" {
