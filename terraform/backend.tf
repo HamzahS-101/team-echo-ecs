853deb99db656444408648d463f7b62317684071
HamzahS-101 <hamzahsahal@gmail.com>
2025-04-13 14:40:11 +0100

    Ran terraform fmt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "team-echo-ecs"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
+    bucket       = "team-echo-ecs"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
     use_lockfile = true
   }
 }
